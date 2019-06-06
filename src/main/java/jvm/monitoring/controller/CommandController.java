c0c8e2f6940d97546148d546bffb99f9166d7e79
heeye <gdshy221@gmail.com>
2019-06-06 18:07:01 +0900

    jvm monitoring 준비

--- src/main/java/jvm/monitoring/controller/CommandController.java
+++ src/main/java/jvm/monitoring/controller/CommandController.java
@@ -2,42 +2,30 @@ package jvm.monitoring.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jvm.monitoring.dto.CommandField;
-import jvm.monitoring.service.StressService;
-
-@RestController("/stress")
+@RestController
+@RequestMapping("/stress")
 public class CommandController {
 
 	public static final Logger logger = LoggerFactory.getLogger(CommandController.class);
 
-	@Autowired
-	public StressService stressService;
-
 	@GetMapping(value = "/go")
-	public String goStressForJVM(@RequestBody CommandField command) throws Throwable {
+	public String goStressForJVM() throws Throwable {
 
-		logger.info("==================== goStressForJVM ====================");
-		logger.info(command.toString());
+		Runtime runtime = Runtime.getRuntime();
+
+		logger.info("==================== heap memory ====================");
+
+		logger.info("totalMemory : {}MB", runtime.totalMemory());
+		logger.info("maxMemory   : {}MB", runtime.maxMemory());
+		logger.info("freeMemory  : {}MB", runtime.freeMemory());
+
 		logger.info("========================================================");
-
-		stressService.goStress(command.getLimitTime(), command.getThreadCount());
 
 		return "go stress";
 	}
 
-	@GetMapping(value = "/stop")
-	public String stopStressForJVM() throws Throwable {
-
-		logger.info("=================== stopStressForJVM ===================");
-		logger.info("========================================================");
-
-		stressService.stopStress();
-		
-		return "stop stress";
-	}
 }
