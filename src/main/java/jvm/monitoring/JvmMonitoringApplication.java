e78a83e795b2c990f9290579ad61f1003e39ebe4
heeye <gdshy221@gmail.com>
2019-06-06 15:02:11 +0900

    init project

---
+++ src/main/java/jvm/monitoring/JvmMonitoringApplication.java
@@ -0,0 +1,13 @@
+package jvm.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmMonitoringApplication.class, args);
+	}
+
+}
